bc61fb02515d586d29b58840116a8847a705c3dc
Yevhenii Mitko <109312584+MitkoYevhenii@users.noreply.github.com>
2023-10-05 15:34:45 +0300

    Відсортував за частотою Map від більшого до меншого(Зміна 3).

--- src/main/java/ua/goit/module10/Task3/WordFrequency.java
+++ src/main/java/ua/goit/module10/Task3/WordFrequency.java
@@ -1,10 +1,11 @@
 package ua.goit.module10.Task3;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 public class WordFrequency {
     public static void main(String[] args) throws FileNotFoundException {
@@ -12,7 +13,7 @@ public class WordFrequency {
         String path = "C:\\Users\\Zhenia\\Documents\\GoIT\\Module10\\src\\main\\java\\ua\\goit\\module10\\Task3\\words.txt";
         File file = new File(path);
         Scanner scanner = new Scanner(file);
-        Map<String, Integer> wordFrequencyMap = new HashMap<>();
+        Map<String, Integer> wordFrequencyMap = new HashMap<String, Integer>();
 
         while (scanner.hasNextLine()) {
             String[] line = scanner.nextLine().split(" ");
@@ -26,11 +27,20 @@ public class WordFrequency {
         }
         scanner.close();
 
-        for (Map.Entry<String, Integer> entry : wordFrequencyMap.entrySet()) {
+        Map<String, Integer> sortedMap = wordFrequencyMap.entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1,
+                        LinkedHashMap::new
+                ));
+
+        for (Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
             String key = entry.getKey();
             Integer value = entry.getValue();
             System.out.println(key + " " + value);
         }
     }
 }
-
